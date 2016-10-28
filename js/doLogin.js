50922a773fc386d54d2902dc83ebe8f7932f90ad
SvenOrdelman <sveno98@live.nl>
2016-10-28 10:24:30 +0200

    login

--- js/doLogin.js
+++ js/doLogin.js
@@ -12,20 +12,26 @@ $(document).ready(function() {
         });
         var username = values["username"];
         var wachtwoord = values["wachtwoord"];
-
+        alert(username + wachtwoord);
         $.ajax({
-            url: "http://localhost:666/api/login",
-            dataType: "json",
-            method: "POST",
-            contentType: 'application/json',
-            data: JSON.stringify({
+            url: 'http://localhost:666/api/login/log',
+            data: {
                 username: username,
                 wachtwoord: wachtwoord
-            })
+            },
+            method: 'POST'
+            //contentType: "application/x-www-form-urlencoded"
         }).fail(function (jqXHR, textStatus) {
             alert("API	Request	failed:	" + textStatus.responseText + jqXHR.responseText);
-        }).done(function (res) {
-            alert(res);
+        }).done(function (data,textStatus,xhr) {
+            switch(xhr.status){
+                case 200:
+                    localStorage.setItem("token", data.token);
+                    break;
+                case 204:
+                    alert("Username or Password incorrect!");
+            }
         });
+        return false;
     });
 });
